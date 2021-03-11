a56c356d7c2fafa1a115518af84a80e6cf7d4f54
russellcoinpay <52371392+russellcoinpay@users.noreply.github.com>
2021-03-12 07:46:22 +0800

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -56,7 +56,7 @@ using namespace boost;
 using namespace std;
 
 namespace {
-    const int MAX_OUTBOUND_CONNECTIONS = 16;
+    const int MAX_OUTBOUND_CONNECTIONS = 8;
 
     struct ListenSocket {
         SOCKET socket;
